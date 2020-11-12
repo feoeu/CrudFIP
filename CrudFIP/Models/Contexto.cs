4ace936fe65e36f6f6aaf916ebd91c3d5ffab217
Fernando Luís Lima Tomé Maia Ribeiro <fernando.ltmr@gmail.com>
2020-11-12 15:05:18 -0300

    Add project files.

---
+++ CrudFIP/Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudFIP.Models
+{
+    public class Contexto : DbContext 
+    {
+        public DbSet<Medico> Medicos { get; set; }
+    }
+}
---
+++ CrudFIP/Models/Medico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CrudFIP.Models
+{
+    public class Medico
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int CodigoCRM { get; set; }
+        public string EstadoCRM { get; set; }
+        public string Especialidade { get; set; }
+    }
+}
